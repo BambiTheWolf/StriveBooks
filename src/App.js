38e230fb19988ca01771fdb44da3edd05a7a9024
Michael Redruello <redruellomichael@gmail.com>
2022-03-24 16:39:25 +0100

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomList from "./components/Booklist";
+import Navbar from "./components/NavBar";
 
 import books from "./data/scifi.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <CustomList books={books} />
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CustomList books={books} />} />
+        </Routes>
+      </BrowserRouter>
+    </header>
+  </div>
+);
 
 export default App;
